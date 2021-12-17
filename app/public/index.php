41b1b9b83ced60966d08acf68c54c720d53cc5bc
Oskar Barcz <oskar.barcz@boldare.com>
2021-12-17 17:54:40 +0100

    chore(ci): app1

--- app/public/index.php
+++ app/public/index.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+declare(strict_types=1);
 
-error_log('HTTP request logs some error.');
+use App\Shared\Infrastructure\Symfony\Kernel;
 
-header('Content-Type: application/json');
-echo json_encode([
-    'message' => 'Hello world!',
-]);
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
