081f750c4bfeea0a9a8482514dadec54dc3ff244
Oskar Barcz <oskar.barcz@boldare.com>
2021-12-29 10:16:40 +0100

    feat(user): add base cqrs entry

---
+++ app/src/User/Application/Command/CreateUserCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Application\Command;
+
+use App\Shared\Application\Command\CommandInterface;
+
+class CreateUserCommand implements CommandInterface
+{
+}
---
+++ app/src/User/Application/Command/CreateUserHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Application\Command;
+
+use App\Shared\Application\Command\CommandHandlerInterface;
+
+class CreateUserHandler implements CommandHandlerInterface
+{
+}
