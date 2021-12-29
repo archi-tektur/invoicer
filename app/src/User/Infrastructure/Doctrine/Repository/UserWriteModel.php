9b6d6151a5e1f4f3de51a7a1838d0aa1e9c7833c
Oskar Barcz <oskar.barcz@boldare.com>
2021-12-29 12:38:35 +0100

    feat(user): add domain methods stubs

---
+++ app/src/User/Infrastructure/Doctrine/Repository/UserWriteModel.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Infrastructure\Doctrine\Repository;
+
+use App\User\Domain\Repository\UserStore;
+use App\User\Domain\User;
+
+class UserWriteModel implements UserStore
+{
+
+    public function get(): User
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function store(User $user): void
+    {
+        // TODO: Implement store() method.
+    }
+}
