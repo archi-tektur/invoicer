bcfcbac304cde13d6bcb83ed13cdae1c07084edb
Oskar Barcz <oskar.barcz@boldare.com>
2021-12-29 10:25:19 +0100

    feat(user): add architools for request processing

---
+++ app/src/Shared/Infrastructure/Request/Exception/MissingRequestParameterException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Request\Exception;
+
+use RuntimeException;
+
+final class MissingRequestParameterException extends RuntimeException
+{
+
+}
