7e857c62ce9507edaa027d274f82d7de0d20aac7
Oskar Barcz <kontakt@archi-tektur.pl>
2022-01-27 22:49:17 +0100

    feat(queue): add command buses abstract logic

--- app/src/Shared/Infrastructure/Symfony/Controller/AbstractAction.php
+++ app/src/Shared/Infrastructure/Symfony/Controller/AbstractAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Shared\Infrastructure\Symfony\Controller;
 
+use App\Shared\Application\Command\CommandBusInterface;
 use App\Shared\Application\Command\CommandInterface;
 use App\Shared\Infrastructure\Symfony\Response\ResponseBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,10 +12,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 abstract class AbstractAction extends AbstractController
 {
     private ResponseBuilder $responseBuilder;
+    private CommandBusInterface $commandBus;
 
-    public function __construct(ResponseBuilder $responseBuilder)
+    public function __construct(ResponseBuilder $responseBuilder, CommandBusInterface $commandBus)
     {
         $this->responseBuilder = $responseBuilder;
+        $this->commandBus = $commandBus;
     }
 
     public function responseBuilder(): ResponseBuilder
@@ -24,5 +27,6 @@ abstract class AbstractAction extends AbstractController
 
     public function do(CommandInterface $command): void
     {
+        $this->commandBus->handle($command);
     }
 }
